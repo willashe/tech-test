cf78ce0ed4b3b71faa183c0febb2b186211e4803
Will Ashe <brantashe@gmail.com>
2021-02-07 15:34:30 -0600

    webpack tooling to support react/scss/babel/typescript/eslint/prettier

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+};
